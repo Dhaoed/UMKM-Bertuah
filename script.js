e35f3ef7258ee6cd0abb68534543b2bbbd4615a3
Vinandra Adam <2101020106@student.umrah.ac.id>
2024-06-21 11:20:47 +0700

    initial commit

---
+++ script.js
@@ -0,0 +1,350 @@
+const umkmData = {
+    kuliner: [
+        {
+            imageSrc: 'assets/images/image_39.jpeg',
+            title: 'Kak Hana Cooking',
+            description: 'Deskripsi Kak Hana Cooking',
+            price: 'Rp. 20.000 - Rp. 50.000',
+            hours: '10.00 - 20.00 WIB',
+            location: 'Jl. Malioboro No. 12, Tanjungpinang',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/image_40.jpeg',
+            title: 'Seafood Gonggong H.Adam',
+            description: 'Deskripsi Seafood Gonggong H.Adam',
+            price: 'Rp. 30.000 - Rp. 80.000',
+            hours: '11.00 - 22.00 WIB',
+            location: 'Jl. Pantai Indah No. 5, Tanjungpinang',
+            phone: '+62 856 7890 1234'
+        }
+    ],
+    busana: [
+        {
+            imageSrc: 'assets/images/fashion.png',
+            title: 'L. Seoul Shop',
+            description: 'Deskripsi L. Seoul Shop',
+            price: 'Rp. 30.000 - Rp. 80.000',
+            hours: '11.00 - 22.00 WIB',
+            location: 'Jl. Pantai Indah No. 5, Tanjungpinang',
+            phone: '+62 856 7890 1234'
+        },
+        {
+            imageSrc: 'assets/images/baju.png',
+            title: 'Anugrah Fashion',
+            description: 'Deskripsi Anugrah Fashion',
+            price: 'Rp. 40.000 - Rp. 90.000',
+            hours: '10.00 - 21.00 WIB',
+            location: 'Jl. Sudirman No. 45, Kijang',
+            phone: '+62 812 3456 7890'
+        }
+    ],
+    kelontong: [
+        {
+            imageSrc: 'assets/images/kelontong2.jpeg',
+            title: 'Toko Sembako Murah',
+            description: 'Deskripsi Toko Sembako Murah',
+            price: 'Rp. 10.000 - Rp. 50.000',
+            hours: '08.00 - 20.00 WIB',
+            location: 'Jl. Mangga No. 1, Kijang',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/kelontong3.png',
+            title: 'Kios Madura',
+            description: 'Deskripsi Kios Madura',
+            price: 'Rp. 5.000 - Rp. 25.000',
+            hours: '07.00 - 19.00 WIB',
+            location: 'Jl. Apel No. 2, Uban',
+            phone: '+62 856 7890 1234'
+        }
+    ],
+    kecantikan: [
+        {
+            imageSrc: 'assets/images/cantik1.jpeg',
+            title: 'Bianca Cosmetic',
+            description: 'Deskripsi Bianca Cosmetic',
+            price: 'Rp. 10.000 - Rp. 50.000',
+            hours: '08.00 - 20.00 WIB',
+            location: 'Jl. Mangga No. 1, Uban',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/image_72.jpeg',
+            title: 'Jelita Estetika',
+            description: 'Deskripsi Jelita Estetika',
+            price: 'Rp. 5.000 - Rp. 25.000',
+            hours: '07.00 - 19.00 WIB',
+            location: 'Jl. Apel No. 2, Uban',
+            phone: '+62 856 7890 1234'
+        }
+    ],
+    pertanian: [
+        {
+            imageSrc: 'assets/images/pertanian1.png',
+            title: 'Atla Agrikultur',
+            description: 'Deskripsi Atla Agrikultur',
+            price: 'Rp. 10.000 - Rp. 50.000',
+            hours: '08.00 - 20.00 WIB',
+            location: 'Jl. Mangga No. 1, Kijang',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/pertanian2.png',
+            title: 'Rejeki Tani',
+            description: 'Deskripsi Rejeki Tani',
+            price: 'Rp. 5.000 - Rp. 25.000',
+            hours: '07.00 - 19.00 WIB',
+            location: 'Jl. Apel No. 2, Jakarta',
+            phone: '+62 856 7890 1234'
+        }
+    ],
+    peternakan: [
+        {
+            imageSrc: 'assets/images/peternakan1.png',
+            title: 'Toko Pakan Ternak Pak Jayadi',
+            description: 'Deskripsi Toko Pakan Ternak Pak Jayadi',
+            price: 'Rp. 10.000 - Rp. 50.000',
+            hours: '08.00 - 20.00 WIB',
+            location: 'Jl. Mangga No. 1, Surabaya',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/peternakan6.png',
+            title: 'Produk Ternak Asri',
+            description: 'Deskripsi Produk Ternak Asri',
+            price: 'Rp. 5.000 - Rp. 25.000',
+            hours: '07.00 - 19.00 WIB',
+            location: 'Jl. Apel No. 2, Jakarta',
+            phone: '+62 856 7890 1234'
+        }
+    ],
+    kebersihan: [
+        {
+            imageSrc: 'assets/images/kebersihan1.jpg',
+            title: 'Perabotan Mang Udjang',
+            description: 'Deskripsi Perabotan Mang Udjang',
+            price: 'Rp. 10.000 - Rp. 50.000',
+            hours: '08.00 - 20.00 WIB',
+            location: 'Jl. Mangga No. 1, Surabaya',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/kebersihan4.jpg',
+            title: 'Bright Shoes',
+            description: 'Deskripsi Bright Shoes',
+            price: 'Rp. 5.000 - Rp. 25.000',
+            hours: '07.00 - 19.00 WIB',
+            location: 'Jl. Apel No. 2, Jakarta',
+            phone: '+62 856 7890 1234'
+        }
+    ],
+    elektronik: [
+        {
+            imageSrc: 'assets/images/elektronik.jpg',
+            title: 'Anugrah Jaya Elektronik',
+            description: 'Deskripsi Anugrah Jaya Elektronik',
+            price: 'Rp. 10.000 - Rp. 50.000',
+            hours: '08.00 - 20.00 WIB',
+            location: 'Jl. Mangga No. 1, Surabaya',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/elektronik1.jpg',
+            title: 'Toko Elektronik Aliong & Son’s',
+            description: 'Deskripsi Toko Elektronik Aliong & Son’s',
+            price: 'Rp. 5.000 - Rp. 25.000',
+            hours: '07.00 - 19.00 WIB',
+            location: 'Jl. Apel No. 2, Jakarta',
+            phone: '+62 856 7890 1234'
+        }
+    ],
+    trending: [
+        {
+            imageSrc: 'assets/images/image_430.png',
+            title: 'Nyamnyang Geprek',
+            description: 'Deskripsi Nyamnyang Geprek',
+            price: 'Rp. 20.000 - Rp. 50.000',
+            hours: '10.00 - 22.00 WIB',
+            location: 'Jl. Raya Cibubur No. 1, Jakarta',
+            phone: '+62 812 3456 7890'
+        },
+        {
+            imageSrc: 'assets/images/kohdaud.jpg',
+            title: 'Toko Koh Daud',
+            description: 'Deskripsi Toko Koh Daud',
+            price: 'Rp. 50.000 - Rp. 500.000',
+            hours: '09.00 - 21.00 WIB',
+            location: 'Jl. Sudirman No. 45, Jakarta',
+            phone: '+62 856 7890 1234'
+        }     
+    ]
+};
+
+function showPopup(data, section) {
+    var popup = document.getElementById("popup");
+    
+    // Set popup content dengan data dari umkmInfo
+    document.getElementById("popup-img").src = data.imageSrc;
+    document.getElementById("popup-title").innerText = data.title;
+    document.getElementById("popup-description").innerText = data.description;
+    document.getElementById("popup-price").innerText = data.price;
+    document.getElementById("popup-hours").innerText = data.hours;
+    document.getElementById("popup-location").innerText = data.location;
+    document.getElementById("popup-phone").innerText = data.phone;
+  
+    popup.style.display = "block";
+}
+
+document.addEventListener('DOMContentLoaded', function () {
+    var popup = document.getElementById("popup");
+    var closeBtn = document.getElementsByClassName("close-btn")[0];
+
+    // Close popup when user clicks on <span> (x)
+    closeBtn.onclick = function() {
+        popup.style.display = "none";
+    }
+
+    // Close popup when user clicks outside of the popup
+    window.onclick = function(event) {
+        if (event.target == popup) {
+            popup.style.display = "none";
+        }
+    }
+
+    // Prevent scrolling when popup is open
+    popup.addEventListener('mousewheel', function(event) {
+        event.preventDefault();
+    });
+
+    // Bind click events for kuliner items
+    var kulinerItems = document.querySelectorAll('.kuliner-item a');
+    kulinerItems.forEach(function(item, index) {
+        if (index < umkmData.kuliner.length) {
+            item.addEventListener('click', function(event) {
+                event.preventDefault();
+                const umkmInfo = umkmData.kuliner[index];
+                showPopup(umkmInfo);
+            });
+        }
+    });
+
+    // Bind click events for busana items
+    var busanaItems = document.querySelectorAll('.busana-item a');
+    busanaItems.forEach(function(item, index) {
+        if (index < umkmData.busana.length) {
+            item.addEventListener('click', function(event) {
+                event.preventDefault();
+                const umkmInfo = umkmData.busana[index];
+                showPopup(umkmInfo);
+            });
+        }
+    });
+
+    // Bind click events for kelontong items
+    var kelontongItems = document.querySelectorAll('.kelontong-item a');
+    kelontongItems.forEach(function(item, index) {
+        if (index < umkmData.kelontong.length) {
+            item.addEventListener('click', function(event) {
+                event.preventDefault();
+                const umkmInfo = umkmData.kelontong[index];
+                showPopup(umkmInfo);
+            });
+        }
+    });
+
+    // Bind click events for kecantikan items
+    var kecantikanItems = document.querySelectorAll('.kecantikan-item a');
+    kecantikanItems.forEach(function(item, index) {
+        if (index < umkmData.kecantikan.length) {
+            item.addEventListener('click', function(event) {
+                event.preventDefault();
+                const umkmInfo = umkmData.kecantikan[index];
+                showPopup(umkmInfo);
+            });
+        }
+    });
+
+    // Bind click events for pertanian items
+    var pertanianItems = document.querySelectorAll('.pertanian-item a');
+    pertanianItems.forEach(function(item, index) {
+        if (index < umkmData.pertanian.length) {
+            item.addEventListener('click', function(event) {
+                event.preventDefault();
+                const umkmInfo = umkmData.pertanian[index];
+                showPopup(umkmInfo);
+            });
+        }
+    });
+
+     // Bind click events for peternakan items
+     var peternakanItems = document.querySelectorAll('.peternakan-item a');
+     peternakanItems.forEach(function(item, index) {
+         if (index < umkmData.peternakan.length) {
+             item.addEventListener('click', function(event) {
+                 event.preventDefault();
+                 const umkmInfo = umkmData.peternakan[index];
+                 showPopup(umkmInfo);
+             });
+         }
+     });
+
+      // Bind click events for kebersihan items
+      var kebersihanItems = document.querySelectorAll('.kebersihan-item a');
+      kebersihanItems.forEach(function(item, index) {
+          if (index < umkmData.kebersihan.length) {
+              item.addEventListener('click', function(event) {
+                  event.preventDefault();
+                  const umkmInfo = umkmData.kebersihan[index];
+                  showPopup(umkmInfo);
+              });
+          }
+      });
+
+       // Bind click events for elektronik items
+       var elektronikItems = document.querySelectorAll('.elektronik-item a');
+       elektronikItems.forEach(function(item, index) {
+           if (index < umkmData.elektronik.length) {
+               item.addEventListener('click', function(event) {
+                   event.preventDefault();
+                   const umkmInfo = umkmData.elektronik[index];
+                   showPopup(umkmInfo);
+               });
+           }
+       });
+ 
+
+     // Bind click events for trending items
+     var trendingItems = document.querySelectorAll('.trending-item img');
+     trendingItems.forEach(function(item, index) {
+         if (index < umkmData.trending.length) {
+             item.addEventListener('click', function(event) {
+                 event.preventDefault();
+                 const umkmInfo = umkmData.trending[index];
+                 showPopup(umkmInfo, 'trending');
+             });
+         }
+     });
+
+    // Add event listener for Enter key press on search input
+    var searchInput = document.getElementById('search-input');
+    searchInput.addEventListener('keypress', function(event) {
+        if (event.key === 'Enter') {
+            event.preventDefault();
+            var query = searchInput.value.toLowerCase();
+            var results = [...umkmData.kuliner, ...umkmData.busana, ...umkmData.kelontong, ...umkmData.kecantikan, ...umkmData.pertanian, ...umkmData.peternakan, ...umkmData.kebersihan, ...umkmData.elektronik, ...umkmData.trending].filter(function(umkm) {
+                return umkm.title.toLowerCase().includes(query) || umkm.description.toLowerCase().includes(query);
+            });
+
+            displayResults(results);
+        }
+    });
+
+    function displayResults(results) {
+        if (results.length > 0) {
+            showPopup(results[0]);
+        } else {
+            alert("UMKM tidak dapat ditemukan");
+        }
+    }
+});
